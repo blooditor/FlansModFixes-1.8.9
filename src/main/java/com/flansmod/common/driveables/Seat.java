1ea6b70afa998a1a683525b4f0205ce11f3d72b6
bld <b>
2020-10-04 00:18:01 +0200

    aimspeed seat change

--- src/main/java/com/flansmod/common/driveables/Seat.java
+++ src/main/java/com/flansmod/common/driveables/Seat.java
@@ -50,7 +50,7 @@ public class Seat {
   /**
    * Yaw/Pitch rotation speeds (Yaw/Pitch/z) where Z is ignored
    */
-  public Vector3f aimingSpeed = new Vector3f(1,1,1);
+  public Vector3f aimingSpeed = new Vector3f(DriveableType.AIM_SPEED_MULTIPLIER,DriveableType.AIM_SPEED_MULTIPLIER,DriveableType.AIM_SPEED_MULTIPLIER);
   /**
    * Where the bullets come from
    */
